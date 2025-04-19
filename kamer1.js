e7d6c10166935bb9d8172c0254aa904d97d2c0c4
Soulreader1 <chris300776@hotmail.com>
2025-04-19 10:03:21 +0200

    Update kamer1.js

--- kamer1.js
+++ kamer1.js
@@ -1,5 +1,1 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    welkom: "Welkom in Kamer 1. Dit is het begin van jouw innerlijke reis. Wat leeft er in jou – en wie leeft jij?",
-  });
-}
+
